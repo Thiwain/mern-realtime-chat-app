a7db1b71f82d366f17df3156faf82d42a97d60c9
Thiwain Medagama <thiwainm@gmail.com>
2024-10-29 13:10:30 +0530

    jwtDecoder

--- server/src/controllers/chat/sendMsgController.ts
+++ server/src/controllers/chat/sendMsgController.ts
@@ -1,10 +1,19 @@
 import { Request, Response, CookieOptions } from "express";
 import {messageValidation} from "../../validation/messageValidation";
 import ChatModel, {ChatItem} from "../../models/chatModel";
+import {jwtDecoder} from "../../utils/jwtDecoder";
 
 export const sendMsgController = async (req:Request,res:Response)=>{
-
     try {
+        const userObj: any = await jwtDecoder(req, res);
+        if (!userObj || !userObj.email) {
+            return res.status(401).json({
+                status: res.statusCode,
+                statusType: 'error',
+                message: 'Failed to authenticate user',
+            });
+        }
+
         const {error}=messageValidation.validate(req.body);
         if (error) {
             return res.status(400).json({
@@ -15,7 +24,7 @@ export const sendMsgController = async (req:Request,res:Response)=>{
         }
 
         await new ChatModel({
-            sentBy:req.body?.email,
+            sentBy:userObj.email,
             message:req.body.message,
         }).save() as ChatItem;
 
