1c8dd26c7510b25776e44337d89eeed7f7a59b86
Thiwain Medagama <thiwainm@gmail.com>
2024-10-29 12:53:53 +0530

    Msg Validation

--- server/src/controllers/auth/signupController.ts
+++ server/src/controllers/auth/signupController.ts
@@ -1,11 +1,11 @@
-import { Request, Response, NextFunction, CookieOptions } from "express";
+import { Request, Response, CookieOptions } from "express";
 import { HashPassword } from "../../utils/passwordUtils";
 import { generateAccessToken, generateRefreshToken } from "../../utils/jwtUtils";
 import UserModel from "../../models/userModel";
 import requestIp from 'request-ip';
 import { signupValidation } from "../../validation/signupValidation";
 
-export const signUpController = async (req: Request, res: Response, next: NextFunction) => {
+export const signUpController = async (req: Request, res: Response) => {
     try {
         const { error } = signupValidation.validate(req.body);
         if (error) {
@@ -67,6 +67,5 @@ export const signUpController = async (req: Request, res: Response, next: NextFu
 
     } catch (err: any) {
         console.error(err);
-        return next(err);
     }
 };
---
+++ server/src/controllers/chat/sendMsgController.ts
@@ -0,0 +1,6 @@
+import { Request, Response, CookieOptions } from "express";
+
+export const sendMsgController = async (req:Request,res:Response)=>{
+
+
+}
