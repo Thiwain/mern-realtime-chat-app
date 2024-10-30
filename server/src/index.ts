0acf82f2430995148bb72469b236d5b594afb9ce
Thiwain Medagama <thiwainm@gmail.com>
2024-10-30 13:56:37 +0530

    web-socket

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,6 +12,7 @@ import {authLimiter} from "./middlewares/authLimiter";
 import MessageRoutes from "./routes/messageRoutes";
 import authenticate from "./middlewares/authenticate";
 import {globalLimiter} from "./middlewares/globalLimiter";
+import WebSocket from 'ws';
 
 dotenv.config();
 
@@ -38,28 +39,46 @@ app.options('*', cors(corsOptions));
 
 // Routes
 app.use('/api/v1/auth/', authLimiter, AuthRoutes);
-app.use('/api/v1/chat/',globalLimiter,MessageRoutes);
+app.use('/api/v1/chat/',globalLimiter,authenticate,MessageRoutes);
 // Routes
 
-// connectDB();
-// const server = app.listen(PORT, () => {
-//     console.log(`Listening on port ${PORT}`);
-// });
-//
-// function gracefulShutdown() {
-//     console.log("Shutting down");
-//     server.close(async () => {
-//         console.log("HTTP server closed.");
-//         try {
-//             await mongoDbCon.close();
-//             console.log('MongoDB connection closed');
-//             process.exit(0);
-//         } catch (error) {
-//             console.error('Error during graceful shutdown:', error);
-//             process.exit(1);
-//         }
-//     });
-// }
-//
-// process.on("SIGTERM", gracefulShutdown);
-// process.on("SIGINT", gracefulShutdown);
+connectDB();
+
+const server = app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws: any) => {
+    console.log('New client connected');
+    ws.on('message', (message: any) => {
+        console.log(`Received: ${message}`);
+        wss.clients.forEach((client: any) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
+function gracefulShutdown() {
+    console.log("Shutting down");
+    server.close(async () => {
+        console.log("HTTP server closed.");
+        try {
+            await mongoDbCon.close();
+            console.log('MongoDB connection closed');
+            process.exit(0);
+        } catch (error) {
+            console.error('Error during graceful shutdown:', error);
+            process.exit(1);
+        }
+    });
+}
+
+process.on("SIGTERM", gracefulShutdown);
+process.on("SIGINT", gracefulShutdown);
